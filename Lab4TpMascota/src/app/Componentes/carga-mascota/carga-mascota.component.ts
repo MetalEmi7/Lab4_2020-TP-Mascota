f6bc7218fad7e2aa5321c5c534f26db61366b577
Emiliano Alvarez <MetalEmi7@gmail.com>
2020-04-07 10:31:24 -0300

    Agregado Componenete carga-mascota

---
+++ Lab4TpMascota/src/app/Componentes/carga-mascota/carga-mascota.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carga-mascota',
+  templateUrl: './carga-mascota.component.html',
+  styleUrls: ['./carga-mascota.component.css']
+})
+export class CargaMascotaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Lab4TpMascota/src/app/app.module.ts
+++ Lab4TpMascota/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CargaMascotaComponent } from './Componentes/carga-mascota/carga-mascota.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CargaMascotaComponent
   ],
   imports: [
     BrowserModule,
